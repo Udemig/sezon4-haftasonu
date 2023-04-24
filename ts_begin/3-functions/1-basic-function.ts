ba9d8d8d345e35d8383f73b9dee36b7457ff949f
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2023-04-24 23:58:35 +0300

    typescript fonksiyon prototip tanimlama

---
+++ ts_begin/3-functions/1-basic-function.ts
@@ -0,0 +1,49 @@
+let total: number;
+
+/**
+ * Typescript'te fonksiyon tanımlamak için üç yöntem kullanılır. Birincisi
+ * function keywordü, ikincisi arrow function, üçüncüsü de single line arrow
+ * function şeklinde.
+ */
+
+// 1. yöntem: `function` keywordü ile fonksiyon tanımlamak
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+total = sum(10, 20);
+console.log("1. total: ", total);
+
+// 2. yöntem: Arrow function tanımlamak
+const add = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+
+total = add(20, 30);
+console.log("2. total: ", total);
+
+// 3. yöntem: Single line arrow function
+const calculate_total = (num1: number, num2: number): number => num1 + num2;
+
+total = calculate_total(30, 40);
+console.log("3. total: ", total);
+
+///////////////////////////////////////////////
+
+/**
+ * Bu bölgeyi mutlaka inceleyin:
+ *
+ * Örnek olarak sıklıkla kullandığımız useSelector fonksiyonunu kullanırken
+ * single line arrow function ile datayı almak yeterlidir. Ama örneğin şöyle bir
+ * senaryo düşünelim: parametreden gelen state değişkenini loglamak istersek
+ * o zaman arrow function haline dönüştürmemiz gerekir.
+ */
+//const authState = useSelector((state) => state.authState);
+//const authState = useSelector((state) => {
+//  console.log(state);
+//  return state.authState;
+//});
+//
+//const authState = useSelector(function getData(state) {
+//  return state.authState;
+//});
---
+++ ts_begin/3-functions/2-function-types.ts
@@ -0,0 +1,171 @@
+/**
+ * Fonksiyonların prototipi fonksiyonun türüdür. Bir fonksiyonun prototipi (türünü)
+ * çözümleyebilmek için (veya tespit edebilmek için) iki ana unsura ihtiyaç vardır.
+ * Birincisi parametreler, ikincisi dönüş türü.
+ *
+ * // TODO Bu yorum satırını sonra güncelle.
+ */
+
+// Aşağıdaki fonksiyonun prototipi tam olarak şu şekildedir:
+// (a: number, b: number) => number
+function toplama_fonksiyonu(a: number, b: number): number {
+  return a + b;
+}
+
+const calculate_sum: (num1: number, num2: number) => number = (
+  num1: number,
+  num2: number
+): number => num1 + num2;
+
+//////////// DETAYLI ÖRNEKLER: ///////////
+
+// ÖRNEK 1: Çift parametreli matematiksel işlemler yapan fonksiyon prototipini tanımlayalım.
+
+// Aşağıdaki tür tanımlama yöntemi farklı şekillerde isimlendirilebilir.
+// - signature
+// - prototype
+// - prototip
+// - fonksiyon türü
+// şeklinde isimler verilebilir.
+
+type DoubleParamMathOperationType = (a: number, b: number) => number;
+
+// Not: Prototipteki parametre isimlerini kullanmak zorunda değiliz ama türlerin sırasına
+// bağlı kalmak zorundayız.
+
+const sumOperation: DoubleParamMathOperationType = (
+  a: number,
+  num2: number
+): number => {
+  return a + num2;
+};
+
+const subtractOperation: DoubleParamMathOperationType = (
+  num1: number,
+  num2: number
+): number => {
+  return num1 - num2;
+};
+
+const multiplicationOperation: DoubleParamMathOperationType = (
+  no1: number,
+  no2: number
+): number => {
+  return no1 * no2;
+};
+
+console.log("sum operation: ", sumOperation(10, 20));
+console.log("subtract operation: ", subtractOperation(10, 20));
+console.log("multiplication op: ", multiplicationOperation(10, 20));
+
+// ÖRNEK 2: Tam isimleri ad-soyad ifadesine çeviren fonksiyon prototipi oluşturalım.
+
+/**
+ * Amaç:
+ *   Bir string ifadeyi (fullname) ad, soyad ve göbek isim olarak ayırmak.
+ *
+ * Şartlar:
+ *   - Türkiyede nüfus kağıdına en fazla iki isim ve bir soyisim yazılabilir.
+ *   - Evli hanımlar iki soyisim alabilir.
+ *   - Mr., Ms., Mrs. gibi ifadeler ünvan olarak ayrılmalı
+ *   - Dört kelimeden sonrası türkiye dışından geldiği düşünülebilir.
+ */
+
+type FullnameParserFuncType = (fullname: string) => string[];
+
+// Örnek 1: Türkiyeli ve bekar insanların isimlerini bulan fonksiyon.
+const turkeyUnmarriedFullnameParser: FullnameParserFuncType = (
+  fullname: string
+): string[] => {
+  const parsedFullname = fullname.split(" ");
+  console.log(">> Parsed fullname", parsedFullname);
+
+  const normalizedParsedFullnames = parsedFullname.filter(
+    (name) => name.length > 0
+  );
+
+  if (
+    normalizedParsedFullnames.length >= 2 &&
+    normalizedParsedFullnames.length <= 3
+  ) {
+    return normalizedParsedFullnames;
+  } else {
+    throw new Error(
+      "Hatalı isim ve soyisim bilgisi girildi, lütfen kontrol ediniz."
+    );
+  }
+};
+
+console.log(turkeyUnmarriedFullnameParser("emir buğra köksalan"));
+// ['emir', 'buğra', 'köksalan']
+
+console.log(turkeyUnmarriedFullnameParser("emir    buğra    köksalan"));
+//["emir", "", "", "", "buğra", "", "", "", "köksalan"];
+
+try {
+  console.log(turkeyUnmarriedFullnameParser(""));
+  // []
+
+  console.log(turkeyUnmarriedFullnameParser("     "));
+  // []
+} catch (err) {
+  console.error(err);
+}
+
+console.log(turkeyUnmarriedFullnameParser("   o  a "));
+// ['o', 'a']
+
+console.log(turkeyUnmarriedFullnameParser("  ahmet    mehmet  "));
+// ['ahmet', 'mehmet']
+
+// Örnek 2: Amerikan uşaklarının isimlerini parçalayan fonksiyon.
+const usaFullnameParser: FullnameParserFuncType = (
+  fullname: string
+): string[] => {
+  const validTitles = ["Mr.", "Ms.", "Mrs."];
+
+  const parsedFullnames = fullname.split(" ");
+  const normalizedParsedFullnames = parsedFullnames.filter((name, index) => {
+    if (index === 0 && validTitles.includes(name)) {
+      return true;
+    }
+
+    if (index > 0 && validTitles.includes(name)) {
+      return false;
+    }
+
+    if (name.length > 0) {
+      return true;
+    }
+
+    return false;
+  });
+
+  if (normalizedParsedFullnames.length <= 4) {
+    return normalizedParsedFullnames;
+  }
+
+  throw new Error("Bu girilen isim Amerika kanunlarına uygun değil.");
+};
+
+console.log(usaFullnameParser("Albert Einstein"));
+console.log(usaFullnameParser("Mr. Bruce Wayne"));
+console.log(usaFullnameParser("Gabriel   Mrs.    Garcia   Marquez"));
+// ['Gabriel', '', '', 'Mrs.', '', '', '', 'Garcia', '', '', 'Marquez']
+
+console.log(usaFullnameParser("Graham Bell    Mr."));
+
+try {
+  console.log(
+    usaFullnameParser(
+      "pablo diego josé francisco de paula juan nepomuceno maría de los remedios cipriano de la santísima trinidad ruiz y picasso"
+    )
+  );
+} catch (err) {
+  console.error(err);
+}
+try {
+  console.log(usaFullnameParser(""));
+} catch (err) {
+  console.error(err);
+}
